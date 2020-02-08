a404e6876687eec2d1840c202441ec208de0618a
pauza <pzedillo@ph.com.mx>
2020-02-08 12:03:03 -0600

    adding rating with stars icons according to rating in database

--- js/main.js
+++ js/main.js
@@ -14,28 +14,37 @@ $.ajax({
     complete: () => {
         console.log('acá termina el spinner');
     }
-})
+});
+
+// hacer el conteo de estrellas pushear el número de estrellas chidas y el de estrellas chafas
+
+const starRating = (stars) => {
+    let totalRating = [];
+    const starPlace = document.getElementById('star-place');
+    for (let i = stars; i >= 1; i--) { console.log(i);
+        totalRating.push('<i class="material-icons sm-icon"> star</i>');
+    };
+    if (5 - stars >= 0) {
+        for (let i = (5 - stars); i >= 1; i--) {
+            totalRating.push('<i class="material-icons sm-icon"> star_border</i>')
+        };
+    };
+    console.log(starPlace.innerHTML = totalRating.join(''));
+};
 
 // Pintar la data en cards 
 const printCard = (data) => {
     const cards = document.getElementById('cards');
     data.map((r) => {
         const cardTemplate = `
+        <div class="col-sm-4">
 <div class="card border-dark mb-3" style="max-width: 18rem;">
 <ul class="list-inline">
-    <li class="list-inline-item">
-        <i class="material-icons sm-icon">
-            star
-        </i>
-        <i class="material-icons sm-icon">
-            star_border
-        </i>
-        <i class="material-icons sm-icon">
-            star_border
-        </i>
-        <i class="material-icons sm-icon">
-            star_border
-        </i>
+    <li class="list-inline-item" id="star-place">
+    <i class="material-icons sm-icon">
+    star
+    </i>
+       
     </li>
 </ul>
 <div class="card-header">
@@ -56,7 +65,7 @@ const printCard = (data) => {
             </i>
         </li>
         <li class="list-inline-item">
-            <h5 class="card-title">Estado: <span id="state">${r.address.state}</span></h5>
+            <p class="card-title">Estado: <br> <span id="state">${r.address.state}</span></p>
         </li>
     </ul>
     <ul class="list-inline">
@@ -66,7 +75,7 @@ const printCard = (data) => {
             </i>
         </li>
         <li class="list-inline-item">
-            <h5 class="card-title">Ciudad: <span id="city">${r.address.city}</span></h5>
+            <p class="card-title">Ciudad: <br> <span id="city">${r.address.city}</span></p>
         </li>
     </ul>
     <ul class="list-inline">
@@ -76,15 +85,17 @@ const printCard = (data) => {
             </i>
         </li>
         <li class="list-inline-item">
-            <h5 class="card-title">Sitio web: <span id="web-site"><a href="${r.contact.site}">${r.contact.site}</a></span></h5>
+            <p class="card-title">Sitio web: <br> <span id="web-site"><a href="${r.contact.site}">${r.contact.site}</a></span></p>
         </li>
     </ul>
 </div>
 </div>
+</div>
 `
         cards.insertAdjacentHTML('afterbegin', cardTemplate);
+        starRating(r.rating);
     });
 };
 
-// hacer el conteo de estrellas pushear el número de estrellas chidas y el de estrellas chafas
+
 // acomodar las tarjetas pa que se vean bonitas
