8ca9189ea963cfe4c3352e407d8e576de79b35df
pauza <pzedillo@ph.com.mx>
2020-02-08 20:09:03 -0600

    finishes sorting by name and rating

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 // Traer la data
 var restaurants = [];
 
+var allCards = '';
 
 const printCard = (data) => {
     data.map((r) => {
         const cardTemplate = `
-        <div class="col-sm-4">
-<div class="card border-dark mb-3" id="${r.id}" style="max-width: 18rem;">
+        <div class="col-lg-4 col-md-6 col-sm-12">
+<div class="card border-dark mb-3" style="max-width: 18rem;">
 <ul class="list-inline">
     <li class="list-inline-item" id="star-place">
     <i class="material-icons sm-icon">
     star
-    </i>
-       
-    </li>
+    </i></li>
+    <li class="list-inline-item"><a href="#more-info" id="${r.id}" class="information btn btn-primary">Más información</a></li>
 </ul>
 <div class="card-header">
     <ul class="list-inline" id="card-header">
@@ -62,6 +62,8 @@ const printCard = (data) => {
 `;
         cards.insertAdjacentHTML('afterbegin', cardTemplate);
         starRating(r.rating);
+        // allCards = document.getElementsByClassName('information');
+        // addButtonEv(allCards);
     });
 };
 
@@ -83,13 +85,6 @@ $.ajax({
     }
 });
 
-// fetch('https://cors-anywhere.herokuapp.com/https://recruiting-datasets.s3.us-east-2.amazonaws.com/data_melp.json')
-// .then((response) => {
-//     console.log(response.blob());
-// }).catch((error) => {
-//     console.log(error.message);
-// })
-// hacer el conteo de estrellas pushear el número de estrellas chidas y el de estrellas chafas
 
 const starRating = (stars) => {
     let totalRating = [];
@@ -97,104 +92,176 @@ const starRating = (stars) => {
     for (let i = stars; i >= 1; i--) {
         totalRating.push('<i class="material-icons sm-icon"> star</i>');
     };
-    if (5 - stars >= 0) {
-        for (let i = (5 - stars); i >= 1; i--) {
+    if (4 - stars >= 0) {
+        for (let i = (4 - stars); i >= 1; i--) {
             totalRating.push('<i class="material-icons sm-icon"> star_border</i>')
         };
     };
     starPlace.innerHTML = totalRating.join('');
 };
-// Pintar la data en cards 
+
+
 const cards = document.getElementById('cards');
 
-
-const printSorted = (sortedArr, data) => {
-    sortedArr.map((r) => {
-        data.forEach(element => {
-            if (element.name == r) {
-                console.log(element);
-                const cardTemplate = `
-                <div class="col-sm-4">
-        <div class="card border-dark mb-3" id="${element.id}" style="max-width: 18rem;">
-        <ul class="list-inline">
-            <li class="list-inline-item" id="star-place">
-            <i class="material-icons sm-icon">
-            star
-            </i>
-               
-            </li>
-        </ul>
-        <div class="card-header">
-            <ul class="list-inline" id="card-header">
-                <li class="list-inline-item">
-                    <i class="material-icons">restaurant</i>
-                </li>
-                <li class="list-inline-item">
-                    <h4 id="restaurant-name">${element.name}</h4>
-                </li>
-            </ul>
-        </div>
-        <div class="card-body text-dark">
+const printSorted = (sortedArr, type, sort) => {
+    cards.innerHTML = '';
+    if (type == 'alphabet') {
+        sortedArr.map((r) => {
+            const cardTemplate = `
+                    <div class="col-sm-4">
+            <div class="card border-dark mb-3" id="${r.id}" style="max-width: 18rem;">
             <ul class="list-inline">
-                <li class="list-inline-item">
-                    <i class="material-icons">
-                        location_city
-                    </i>
+                <li class="list-inline-item" id="star-place">
+                <i class="material-icons sm-icon">
+                star
+                </i>
                 </li>
-                <li class="list-inline-item">
-                    <p class="card-title">Estado: <br> <span id="state">${element.address.state}</span></p>
-                </li>
+                <li class="list-inline-item"><a href="#more-info" id="${r.id}" class="information btn btn-primary">Más información</a></li>
             </ul>
-            <ul class="list-inline">
-                <li class="list-inline-item">
-                    <i class="material-icons">
-                        house
-                    </i>
-                </li>
-                <li class="list-inline-item">
-                    <p class="card-title">Ciudad: <br> <span id="city">${element.address.city}</span></p>
-                </li>
-            </ul>
-            <ul class="list-inline">
-                <li class="list-inline-item">
-                    <i class="material-icons">
-                        web
-                    </i>
-                </li>
-                <li class="list-inline-item">
-                    <p class="card-title">Sitio web: <br> <span id="web-site"><a href="${element.contact.site}">${element.contact.site}</a></span></p>
-                </li>
-            </ul>
-        </div>
-        </div>
-        </div>`;
-        cards.insertAdjacentHTML('afterbegin', cardTemplate);
-        starRating(r.rating);
+            <div class="card-header">
+                <ul class="list-inline" id="card-header">
+                    <li class="list-inline-item">
+                        <i class="material-icons">restaurant</i>
+                    </li>
+                    <li class="list-inline-item">
+                        <h4 id="restaurant-name">${r.name}</h4>
+                    </li>
+                </ul>
+            </div>
+            <div class="card-body text-dark">
+                <ul class="list-inline">
+                    <li class="list-inline-item">
+                        <i class="material-icons">
+                            location_city
+                        </i>
+                    </li>
+                    <li class="list-inline-item">
+                        <p class="card-title">Estado: <br> <span id="state">${r.address.state}</span></p>
+                    </li>
+                </ul>
+                <ul class="list-inline">
+                    <li class="list-inline-item">
+                        <i class="material-icons">
+                            house
+                        </i>
+                    </li>
+                    <li class="list-inline-item">
+                        <p class="card-title">Ciudad: <br> <span id="city">${r.address.city}</span></p>
+                    </li>
+                </ul>
+                <ul class="list-inline">
+                    <li class="list-inline-item">
+                        <i class="material-icons">
+                            web
+                        </i>
+                    </li>
+                    <li class="list-inline-item">
+                        <p class="card-title">Sitio web: <br> <span id="web-site"><a href="${r.contact.site}">${r.contact.site}</a></span></p>
+                    </li>
+                </ul>
+            </div>
+            </div>
+            </div>`;
+            if (sort == 'descendent') {
+                cards.insertAdjacentHTML('beforeend', cardTemplate);
+            } else {
+                cards.insertAdjacentHTML('afterbegin', cardTemplate);
             }
-        });
-    })
+            starRating(r.rating);
+        })
+    } else if (type == 'rating') {
+        console.log('ordenando por rating');
+        sortedArr.map((r) => {
+                const cardTemplate = `
+             <div class="col-sm-4">
+                <div class="card border-dark mb-3" style="max-width: 18rem;">
+                <ul class="list-inline">
+                <li class="list-inline-item" id="star-place">
+                    <i class="material-icons sm-icon">star</i> 
+                </li>
+                <li class="list-inline-item"><a href="#more-info" id="${r.id}" class="information btn btn-primary">Más información</a></li>
+                </ul>
+            <div class="card-header">
+                <ul class="list-inline" id="card-header">
+                    <li class="list-inline-item">
+                        <i class="material-icons">restaurant</i>
+                    </li>
+                    <li class="list-inline-item">
+                        <h4 id="restaurant-name">${r.name}</h4>
+                    </li>
+                </ul>
+            </div>
+            <div class="card-body text-dark">
+                <ul class="list-inline">
+                    <li class="list-inline-item">
+                        <i class="material-icons">location_city</i>
+                    </li>
+                    <li class="list-inline-item">
+                        <p class="card-title">Estado: <br> <span id="state">${r.address.state}</span></p>
+                    </li>
+                </ul>
+                <ul class="list-inline">
+                    <li class="list-inline-item">
+                        <i class="material-icons">house</i>
+                    </li>
+                    <li class="list-inline-item">
+                        <p class="card-title">Ciudad: <br> <span id="city">${r.address.city}</span></p>
+                    </li>
+                </ul>
+                <ul class="list-inline">
+                    <li class="list-inline-item">
+                        <i class="material-icons">web</i>
+                    </li>
+                    <li class="list-inline-item">
+                        <p class="card-title">Sitio web: <br> <span id="web-site"><a href="${r.contact.site}">${r.contact.site}</a></span></p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>`;
+                cards.insertAdjacentHTML('afterbegin', cardTemplate);
+                starRating(r.rating);
+            })
+        }
 }
 
 // sort alphabetically
 const alphabetically = (val) => {
-    console.log(restaurants);
-    var restNames = [];
-    restaurants.map((e) => {
-        restNames.push(e.name);
-    })
-    console.log(restaurants);
-    console.log(restNames);
     cards.innerHTML = '';
+    restaurants.sort((a, b) => {
+        if (a.name < b.name) {
+            return -1
+        } else if (a.name > b.name) {
+            return 1
+        } else {
+            return 0
+        }
+    });
+    console.log(restaurants);
     if (val == 2) {
-        restNames.sort();
-        printSorted(restNames, restaurants);
+        sorting = 'ascendent';
     } else {
-        restNames.sort();
-        restNames.reverse();
-        printSorted(restNames, restaurants);
+        sorting = 'descendent';
     }
-    console.log(restNames);
+    printSorted(restaurants, 'alphabet', sorting);
 };
+
+const starsNumber = (val) => {
+    cards.innerHTML = '';
+    if (val == 3) {
+        restaurants.sort((a, b) => {
+            return (a.rating - b.rating)
+        });
+        console.log(restaurants);
+        printSorted(restaurants, 'rating')
+    } else {
+        restaurants.sort((a, b) => {
+            return (b.rating - a.rating)
+        });
+        console.log(restaurants);
+        printSorted(restaurants, 'rating')
+    };
+}
 
 const sortSelect = document.getElementById('inlineFormCustomSelectPref');
 
@@ -203,6 +270,45 @@ sortSelect.addEventListener('change', () => {
     if (valueSelected == 1 || valueSelected == 2) {
         alphabetically(valueSelected);
     } else {
-        ranking(valueSelected);
+        starsNumber(valueSelected);
     }
 });
+
+// maps
+// Initialize and add the map
+const initMap = (lat, long) => {
+    // The location of Uluru
+    const coords = {
+        lat: lat,
+        lng: long
+    };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 4,
+            center: coords
+        });
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({
+        position: coords,
+        map: map
+    });
+}
+
+// const addButtonEv = (buttons) => {
+//     for (let i = 0; i < buttons.length; i++) {
+//         buttons[i].addEventListener('click', () => {
+//             console.log(buttons[i]);
+//         })
+//     };
+// };
+
+$(window).on('load', () => {
+    const buttons = $('.information');
+    console.log($('.information'))
+    $(buttons).each((e) => {
+        buttons[e].click(() => {
+            console.log(buttons[e]);
+        })
+    })
+})
